5eb2cc2a0e2730b818bed734cea8560fd681747d
mnk <storrymn@gmail.com>
2025-02-17 14:19:03 +0900

    공통사항 관련 ing

--- src/js/common.js
+++ src/js/common.js
@@ -1,9 +1,105 @@
 (function() {  
   console.log('common.js');
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   const elTitle = document.querySelector('title').innerHTML = 'common.js';
-  // });
+
+  const $DOM = $(document),
+        $WIN = $(window),
+        wHeight = $WIN.height();
+
+
+  /* 전체메뉴 */
+  $DOM.on('click', '.header .right [class^=allmenu]', function(){
+    const $this = $(this),
+          nav = $this.closest('.header_inner').find('.nav_menu_wrap');
+
+    if(nav.css('visibility') == 'hidden'){
+      nav.addClass('active').attr('aria-hidden', 'false');
+    }else {
+      nav.removeClass('active').attr('aria-hidden', 'true');
+      $('.header .allmenu').focus();
+    }
+  });
+
+
+  /* Popup 관련 */
+  // 팝업 열기
+  $.fn.openPop = function(){
+    $(this).addClass('active');
+  }
+  // 팝업 닫기
+  $.fn.openPop = function(){
+    $(this).removeClass('active');
+    $(this).closest('.popup_wrap').removeClass('active');
+    $(this).focus();
+  }
+
+  $('.popup_open').on('click', function(){
+    const $this = $(this).data('popup-id');
+    $('#' + $this).openPop();
+  });
+  $('.popup_close').on('click', function(){
+    $(this).closePop();
+  });
+
+
+  /* Accordian */
+  $DOM.on('click', '.acd_wrap .acd_head', function(){
+    const $this = $(this),
+          $acd_bottom = $this.next('.acd_cont'),
+          $click_item = $this.closest('.acd_item');
+
+    $click_item.children('.acd_head').attr('aria-expanded', 'false');
+    $click_item.children('.acd_cont').stop(true,true).slideUp();
+
+    if($acd_bottom.css('display') == 'none'){
+      $this.attr('aria-expanded', 'true');
+      $acd_bottom.stop(true, true).slideDown();
+    }
+  })
+
+
+  /* Tooltip */
+  $DOM.on('click', '.tooltip_wrap button', function(){
+    const $click = $(this).closest('.tooltip_wrap'),
+          $t_text = $click.find('.tooltip_text');
+    
+    if($t_text.css('display' == 'none')){
+      $t_text.show();
+    }else{
+      $t_text.hide();
+    }
+  })
+
+  $DOM.on('blur keyup', '.byte_check > textarea', function(){
+    const str = $(this).val(),
+          strLng = str.length,
+          rbyte = 0,
+          rlen = 0,
+          one_char = '',
+          str2 = '',
+          maxByte = 500;
+
+    for(const i=0; i<strLng; i++){
+      one_char = str.charAt(i);
+      if(escape(one_char).length > 4){
+        rbtye += 2;
+      }else {
+        rbyte++;
+      }
+
+      if(rbyte <= maxByte){
+        rlen = i+1;
+      }
+    }
+
+    if(rbyte > maxByte){
+      str2 = str.substr(0,rlen);
+      $(this).val(str2);
+    }else {
+      $(this).next().find('em').html(rbyte);
+    }
+  })
 })();
+
 $(function(){
   console.log('jquery ready');
 });
