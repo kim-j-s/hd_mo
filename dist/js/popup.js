2b97fcc9fb5f2a799efad2f6638dfb979e1a13db
mnk <storrymn@gmail.com>
2025-03-28 13:16:03 +0900

    popup js 제거

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -1,84 +1,13 @@
 /* Popup 관련 */
 
 // Popup 열기
-function openPop($triggerEl, target) {
+function openPop(target) {
 	const $target = $("#" + target);
-
-	if ($target.length) {
-		const getOpenerId = $($triggerEl).attr("triggerId");
-		let openerId;
-
-		if (!getOpenerId) {
-			openerId = generateUUID();
-			$($triggerEl).attr("triggerId", openerId);
-		} else {
-			openerId = getOpenerId;
-		}
-
-		console.log("현재팝업 open");
-		$target.attr("opner", openerId);
-		$("body").css("overscroll-behavior", "contain").addClass("scroll_lock");
-		$target.addClass("active");
-
-		//렌더링 후, focus 이동
-		setTimeout(function () {
-			const $pop_header = $target.find(".popup_inner").children(".popup_head"),
-				$pop_cont = $target.find(".popup_inner").children(".popup_cont");
-
-			if ($pop_header.length) {
-				$pop_header.attr("tabindex", "0").focus();
-			} else {
-				$pop_cont.attr("tabindex", "0").focus();
-			}
-			// $target.find('.popup_inner').attr('tabindex', '0').focus();
-			$(".popup_wrap.active").attr("aria-hidden", true);
-			$(".wrap").attr("aria-hidden", true);
-			$target.attr("aria-hidden", false);
-			console.log("현재팝업 focus");
-		}, 600);
-	}
-
-	// bottom 팝업 - drag
-	if ($target.hasClass("bottom")) {
-		draggable($target);
-	}
 }
 
 // Popup 닫기
 function closePop(target) {
 	const $target = $("#" + target);
-	const $opener = $('[triggerId="' + $target.attr("opner") + '"]');
-
-	if ($target.hasClass("active")) {
-		$target.removeClass("active").attr("opner", null);
-
-		console.log("현재팝업 close");
-		// $('body').removeAttr('style');
-
-		const $lastPopup = $(".popup_wrap.active:last");
-		if ($lastPopup.length) {
-			$lastPopup.attr("aria-hidden", false);
-
-			setTimeout(function () {
-				$lastPopup.find(".popup_inner").attr("tabindex", "0").focus();
-				$target.attr("aria-hidden", true);
-				$target.find(".popup_inner").removeAttr("tabindex");
-				// console.log('이전팝업에 focus');
-			}, 600);
-		}
-
-		const popup_count = $(".popup_wrap.active").length;
-		if (popup_count <= 0) {
-			$("body").removeClass("scroll_lock");
-			setTimeout(() => {
-				$opener.focus();
-				// console.log('클릭한 버튼으로 다시 focus');
-				$("body").removeClass("scroll_lock");
-				$(".wrap").attr("aria-hidden", false);
-				$(".popup_wrap").attr("aria-hidden", true);
-			}, 600);
-		}
-	}
 }
 
 // 팝업 영역 외 클릭 시 팝업 닫기
