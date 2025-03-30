ee1fab82bc966e21e414212a5fa0ebfbba3eeb60
kim-j-s <kogwing@gmail.com>
2025-03-31 08:53:12 +0900

    ing

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -146,16 +146,3 @@ function toastMsg(msg) {
 	// const text = $('<div class='toast_msg'></div>').text(msg);
 	$(".toast_msg").text(msg);
 }
-
-// UUID생성
-/* 추후
-function generateUUID() {
-	var d = new Date().getTime();
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-		var r = (d + Math.random()*16)%16 | 0;
-		d = Math.floor(d/16);
-		return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-	});
-	return uuid;
-};
-*/
