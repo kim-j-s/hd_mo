9365f1ab63ca8ecc1a0876b2a4e64c9a79b5a105
BrynnaWee <brynna.we@gmail.com>
2025-04-14 15:03:28 +0900

    dist

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -16,7 +16,7 @@ class HD_Popup {
 		this.$popup_dim = this.$target.find(".popup_dim");
 		this.$triggerEl = $triggerEl;
 		this.$closeBtn = this.$target.find(".popup_close");
-		this.$header = this.$target.find(".popup_head").length > 0 ? this.$target.find(".popup_head") : null;
+		this.$header = this.$target.find(".popup_head_title").length > 0 ? this.$target.find(".popup_head_title") : null;
 		this.$content = this.$target.find(".popup_cont").length > 0 ? this.$target.find(".popup_cont") : null;
 	}
 
@@ -25,6 +25,8 @@ class HD_Popup {
 			$(".wrap").attr("aria-hidden", "true");
 			$(".wrap").attr("inert", "");
 		}
+
+		console.log(this.$target.find(".popup_head_title"));
 
 		//popup active
 		this.isOpen = true;
@@ -100,7 +102,7 @@ class HD_Popup {
 
 		const $prevPopup = this.$triggerEl.closest(".popup_wrap.active");
 		if ($prevPopup) {
-			const $prevHeader = $($prevPopup).find(".popup_head").length > 0 ? $($prevPopup).find(".popup_head") : null;
+			const $prevHeader = $($prevPopup).find(".popup_head_title").length > 0 ? $($prevPopup).find(".popup_head_title") : null;
 			const $prevContent = $($prevPopup).find(".popup_cont").length > 0 ? $($prevPopup).find(".popup_cont") : null;
 
 			$($prevPopup).attr("aria-hidden", "false");
@@ -177,7 +179,7 @@ function attachPopup(newPopup) {
 				<div class="popup_container">
 					<div class="popup_dim" aria-hidden="true"></div>
 					<div class="popup_inner">
-						<div class="popup_head">팝업 타이틀</div>
+						<div class="popup_head"><div class="popup_head_title">팝업 타이틀</div></div>
 						<div class="popup_cont">
 							<div class="popup_content">
 								<!-- s: popup_content_top -->
@@ -224,247 +226,6 @@ function attachPopup(newPopup) {
 				</div>
 			</div>`;
 
-
-	//Layer 팝업
-	window.popupGroup.pop_type02 = `<div class="popup_wrap layer" id="pop_type02" role="dialog" aria-hidden="true">
-				<div class="popup_container">
-					<div class="popup_dim" aria-hidden="true"></div>
-					<div class="popup_inner">
-						<div class="popup_head">팝업 타이틀</div>
-						<div class="popup_cont">
-							<div class="popup_content">
-								<!-- s: popup_content_top -->
-								<div class="popup_content_top">
-									<h2 class="title_h2">타이틀영역</h2>
-								</div>
-								<!-- e: popup_content_top -->
-								팝업내용입니다
-								<br />
-								내용
-							</div>
-						</div>
-						<div class="popup_bottom">
-							<div class="btn_area">
-								<button type="button" class="hd_btn hd_btn_ty_3 hd_btn_s_2">
-									<span class="text">취소</span>
-								</button>
-								<button type="button" class="hd_btn hd_btn_ty_1 hd_btn_s_2">
-									<span class="text">확인</span>
-								</button>
-							</div>
-						</div>
-						<button type="button" class="popup_close"><span class="blind">팝업 닫기</span></button>
-					</div>
-				</div>
-			</div>`;
-
-
-	//pop_type03
-	window.popupGroup.pop_type03 = `<div class="popup_wrap bottom" id="pop_type03" role="dialog" aria-modal="true" aria-hidden="true">
-				<div class="popup_container">
-					<div class="popup_dim" aria-hidden="true"></div>
-					<div class="popup_inner">
-						<div class="draggable" aria-hidden="true">
-							<div class="move"></div>
-						</div>
-						<div class="popup_head">팝업 타이틀</div>
-						<div class="popup_cont">
-							<div class="popup_content">
-								<!-- s: popup_content_top -->
-								<div class="popup_content_top">
-									<h2 class="title_h2">타이틀영역</h2>
-								</div>
-								<!-- e: popup_content_top -->
-								팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								내용 팝업내용입니다
-								<br />
-								팝업내용입니다
-							</div>
-						</div>
-						<div class="popup_bottom">
-							<div class="btn_area">
-								<button type="button" class="hd_btn hd_btn_ty_2 hd_btn_s_2">
-									<span class="text">확인</span>
-								</button>
-								<button type="button" class="hd_btn hd_btn_ty_1 hd_btn_s_2" onclick="openPop(this,'pop_type04');" title="Bottom 팝업열기">
-									<span class="text">Bottom 팝업열기</span>
-								</button>
-							</div>
-						</div>
-						<button type="button" class="popup_close"><span class="blind">팝업 닫기</span></button>
-					</div>
-				</div>
-			</div>`;
-			
-
-		//pop_type04
-		window.popupGroup.pop_type04 = `<div class="popup_wrap bottom" id="pop_type04" role="dialog" aria-modal="true" aria-hidden="true">
-			<div class="popup_container">
-				<div class="popup_dim" aria-hidden="true"></div>
-				<div class="popup_inner">
-					<div class="draggable" aria-hidden="true">
-						<div class="move"></div>
-					</div>
-					<div class="popup_head"></div>
-					<div class="popup_cont">
-						<div class="popup_content">
-							<!-- s: popup_content_top -->
-							<div class="popup_content_top">
-								<h2 class="title_h2">타이틀영역</h2>
-							</div>
-							<!-- e: popup_content_top -->
-							팝업내용입니다
-							<br />
-							내용 팝업내용입니다
-							<br />
-							팝업내용입니다
-							<br />
-							내용 팝업내용입니다
-							<br />
-							내용 팝업내용입니다
-							<br />
-							내용 팝업내용입니다
-							<br />
-						</div>
-					</div>
-					<div class="popup_bottom">
-						<div class="btn_area">
-							<button type="button" class="hd_btn hd_btn_ty_2 hd_btn_s_2">
-								<span class="text">확인</span>
-							</button>
-							<button type="button" class="hd_btn hd_btn_ty_1 hd_btn_s_2" onclick="openPop(this,'pop_type05');" title="Bottom 팝업열기">
-								<span class="text">Bottom 팝업열기</span>
-							</button>
-						</div>
-					</div>
-					<button type="button" class="popup_close"><span class="blind">팝업 닫기</span></button>
-				</div>
-			</div>
-		</div>`;
-
-
-		//pop_type05
-		window.popupGroup.pop_type05 = `<div class="popup_wrap bottom" id="pop_type05" role="dialog" aria-modal="true" aria-hidden="true">
-			<div class="popup_container">
-				<div class="popup_dim" aria-hidden="true"></div>
-				<div class="popup_inner">
-					<div class="draggable" aria-hidden="true">
-						<div class="move"></div>
-					</div>
-					<div class="popup_head">팝업 타이틀</div>
-					<div class="popup_cont">
-						<div class="popup_content">
-							<div class="opt_select_list opt_case1">
-								<div role="button" class="option active" title="선택됨"><span class="text">K7 Premier 2.5 GDI</span></div>
-								<div role="button" class="option"><span class="text">K7 Premier 2.5 GDI</span></div>
-								<div role="button" class="option"><span class="text">K7 Premier 2.5 GDI</span></div>
-								<div role="button" class="option"><span class="text">K7 Premier 2.5 GDI</span></div>
-								<div role="button" class="option"><span class="text">K7 Premier 2.5 GDI</span></div>
-								<div role="button" class="option"><span class="text">K7 Premier 2.5 GDI</span></div>
-							</div>
-						</div>
-					</div>
-					<div class="popup_bottom">
-						<div class="btn_area">
-							<button type="button" class="hd_btn hd_btn_ty_1 hd_btn_s_2">
-								<span class="text">확인</span>
-							</button>
-						</div>
-					</div>
-					<button type="button" class="popup_close"><span class="blind">팝업 닫기</span></button>
-				</div>
-			</div>
-		</div>`;
-
-	//pop_type06
-	window.popupGroup.pop_type06 = `<div class="popup_wrap bottom" id="pop_type06" role="dialog" aria-modal="true" aria-hidden="true">
-				<div class="popup_container">
-					<div class="popup_dim" aria-hidden="true"></div>
-					<div class="popup_inner">
-						<div class="draggable" aria-hidden="true">
-							<div class="move"></div>
-						</div>
-						<div class="popup_head">팝업 타이틀</div>
-						<div class="popup_cont">
-							<div class="popup_content">
-								<div class="input_text">
-									<div class="inp">
-										<input type="text" placeholder="추가부속품 명을 입력해 보세요" title="추가부속품 명을 입력해 보세요" />
-										<button type="button" class="del"><span class="blind">텍스트 삭제</span></button>
-									</div>
-								</div>
-								<div class="opt_select_list opt_case2">
-									<div role="button" class="option active" title="선택됨">
-										<span class="text">
-											<span class="point">4륜</span>
-											구동
-										</span>
-									</div>
-									<div role="button" class="option"><span class="text">ABS</span></div>
-									<div role="button" class="option"><span class="text">Around View Monitor 패키지(단품)</span></div>
-									<div role="button" class="option fixed"><span class="text">Around View Monitor 패키지(직접입력)</span></div>
-									<div role="button" class="option"><span class="text">ECM 사이드/룸미러</span></div>
-									<div role="button" class="option"><span class="text">Head Up Display</span></div>
-								</div>
-							</div>
-						</div>
-						<div class="popup_bottom">
-							<div class="btn_area">
-								<button type="button" class="hd_btn hd_btn_ty_1 hd_btn_s_2">
-									<span class="text">확인</span>
-								</button>
-							</div>
-						</div>
-						<button type="button" class="popup_close"><span class="blind">팝업 닫기</span></button>
-					</div>
-				</div>
-			</div>`;
-
-
-	//pop_type07
-	window.popupGroup.pop_type07 = `<div class="popup_wrap bottom" id="pop_type07" role="dialog" aria-modal="true" aria-hidden="true">
-			<div class="popup_container">
-				<div class="popup_dim" aria-hidden="true"></div>
-				<div class="popup_inner">
-					<div class="draggable" aria-hidden="true">
-						<div class="move"></div>
-					</div>
-					<div class="popup_head">팝업 타이틀</div>
-					<div class="popup_cont">
-						<div class="popup_content">
-							<div class="opt_select_list opt_case3">
-								<div role="button" class="option active" title="선택됨"><span class="text">항목 1</span></div>
-								<div role="button" class="option"><span class="text">항목 2</span></div>
-								<div role="button" class="option"><span class="text">항목 3</span></div>
-								<div role="button" class="option"><span class="text">항목 4</span></div>
-								<div role="button" class="option"><span class="text">항목 5</span></div>
-								<div role="button" class="option"><span class="text">항목 6</span></div>
-							</div>
-						</div>
-					</div>
-					<button type="button" class="popup_close"><span class="blind">팝업 닫기</span></button>
-				</div>
-			</div>
-		</div>`;
 	}
 </script> */
 }
