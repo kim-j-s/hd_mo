dd995b9fab89b79f201d61673e9af1537d80da20
mnk <storrymn@gmail.com>
2025-09-11 13:21:02 +0900

    dist test

--- dist/js/common.js
+++ dist/js/common.js
@@ -324,10 +324,10 @@
 					$count = $(this).closest('.length_check').find('.counter em');
 
     if(str.length == 0 || str == ''){
-      $count.html('<span class="sr-only">현재 입력한 숫자의 수</span>' + '0');
+      $count.html('<span class="live_region">현재 입력한 숫자의 수</span>' + '0');
     }else{
 			// $count.text(str.length);
-      $count.html('<span class="sr-only">현재 입력한 숫자의 수</span>' + str.length);
+      $count.html('<span class="live_region">현재 입력한 숫자의 수</span>' + str.length);
     }
 
     if (str.length > 500) {
